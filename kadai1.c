9bd2fe04599a96da1e6aef1e842d3b1596a3cfc8
r2210745 <r2210745@a62.ied.inf.uec.ac.jp>
2024-04-09 11:43:44 +0900

    Testing

--- kadai1.c
+++ kadai1.c
@@ -7,10 +7,16 @@
 
 int main(){
     FILE *fptr;
+    
     fptr = fopen("input.txt", "r");
     char myString[100];
-    fgets(myString, 100, fptr);
-    printf("%s", myString);
+    // fgets(myString, 100, fptr);
+    // The first parameter specifies where to store the file content, which will be in the 'myString' array
+    // The second parameter specifies the maximum size of data to read, which is myString(100)
+    // The third parameter requires a file pointer that is used to read the file
+    while(fgets(myString, 100, fptr)){
+        printf("%s", myString);
+    }
     fclose(fptr);
 
     return 0;
