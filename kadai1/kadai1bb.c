4a26d80ebee2991e83e9ea38716d92e695c7a3ff
David Leora <liuleora@gmail.com>
2024-04-09 14:21:13 +0900

    Completed kadai1bb.c

--- kadai1/kadai1bb.c
+++ kadai1/kadai1bb.c
@@ -15,9 +15,10 @@ typedef struct node_tag {
     struct node_tag *next; // 次のノードへのポインタ
 } node_type;
 
-void printList(node_type *head){
+void printList(node_type *head, double sum){
     node_type *current = head;
     while(current != NULL){
+        sum += current->data;
         printf("%lf", current->data);
         if (current -> next != NULL)
             printf(" -> ");
@@ -25,10 +26,21 @@ void printList(node_type *head){
             printf("\n");
         current = current->next;
     }
+    printf("The sum of the numbers is: %lf\n", sum);
+}
+
+int search_position(data_type x, node_type *p){
+    int i = 0;
+    while((p != NULL) && (fabs(x) > fabs(p->data))){
+        p = p->next;
+        i++;
+    }
+    return i;
 }
 
 int main(){
     FILE *fptr;
+    int pos;
     double sum = 0.0;
     
     node_type *head = NULL;
@@ -38,14 +50,26 @@ int main(){
     while(fscanf(fptr, "%lf", &num) != EOF){
         node_type *new_node = (node_type *)malloc(sizeof(node_type));
         new_node->data = num;
-        new_node->next = head;
-        head = new_node;
-        sum += num;
+        new_node->next = NULL;
+
+        pos = search_position(num, head);
+
+        if (pos == 0 || head == NULL){
+            // Insert at the beginning of the list
+            new_node->next = head;
+            head = new_node;
+        } else {
+            // Insert at the correct position
+            node_type *current = head;
+            for(int i=0; i < pos-1 && current->next != NULL; i++){
+                current = current->next;
+            }
+            new_node->next = current->next;
+            current->next = new_node;
+        }
     }
 
-    printList(head);
-
+    printList(head, sum);
     fclose(fptr);
-    printf("The sum of the numbers is: %lf\n", sum);
     return 0;
 }
